67c50dd6dfbf9e1a8a67116d699548bf53412aa9
rabie01 <rabeea2100@yahoo.com>
2026-02-17 14:18:51 -0600

    fix app-deploy

--- iac/modules/ecr/main.tf
+++ iac/modules/ecr/main.tf
@@ -4,7 +4,7 @@
 
 resource "aws_ecr_repository" "app" {
   name                 = "${var.app_name}-app"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
