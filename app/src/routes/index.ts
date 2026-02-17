712f18561642ac0ecc6fceb5afa1001fb3b1ef36
rabie01 <rabeea2100@yahoo.com>
2026-02-17 13:50:55 -0600

    Fix greeting message in index route

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (_req, res) => {
-  res.send('Hello from Express + TypeScript!!!codedeploy');
+  res.send('Hello from Express + TypeScript!codedeploy');
 });
 
 // Health check endpoint
